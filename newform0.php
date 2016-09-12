435e540c966aff3605124936e28f66092e6f71ac
Harry Zhang <hnzhang@gmail.com>
2016-09-11 22:07:38 -0700

    all data are ready dropdown list are populated order details can be displayed

--- newform0.php
+++ newform0.php
@@ -1,7 +1,6 @@
 <?php
 $inventory = array("categories"=>array( ), "vendors"=>array());
 
-
 $AccountEmail = "";
 $VisibleCtl = false;
 function getAccountEmail() {
@@ -16,26 +15,46 @@ function getAccountEmail() {
 		}
 	}
 }
-$generalMessage = "";
-$pickupOptions = array();
+$Special_Messages = "";
+$PickupOptions = array();
+$Order_Deadline = "";
 /*
 	get template data for general instruction, deadline and pickup, etc
 */
 function getTemplateInfo() {
+	global $Special_Messages, $PickupOptions, $Order_Deadline;
+	$template_url= "https://spreadsheets.google.com/feeds/list/1Kp0Lcneb_UUjE3Vi0FjpCNbXdTRLATjSpyNnLJx-E9Y/1/public/values?alt=json";
+	$json = file_get_contents($template_url);
+	$data = json_decode($json, TRUE);
+	$rows = $data['feed']['entry'];
+	foreach ($rows as $item) {
+		$opt = $item['gsx$pickups']['$t'];
+		$opt = trim($opt);
+		if (strlen($opt) > 0) {
+				array_push($PickupOptions, $opt);
+				//pickups.append(jQueryLatest("<option>").attr("value", opt).text(opt))
+		}
+		$nextOrderData = trim($item['gsx$nextorderdate']['$t']);
+		if (strlen($nextOrderData) >0) {
+				$Order_Deadline = $nextOrderData;
+		}
 
+		$msg = trim($item['gsx$specialmessages']['$t']);
+		if (strlen($msg)) {
+			$Special_Messages = $msg;
+		}
+	}
 }
 /*
 	build up  inventory data
 */
 function getInventoryInfo() {
-	global $AccountEmail, $inventory;
-	echo  "EMail:". $AccountEmail.'<br>';
+	global $inventory;
 	$url_inventory = "https://spreadsheets.google.com/feeds/list/1Kp0Lcneb_UUjE3Vi0FjpCNbXdTRLATjSpyNnLJx-E9Y/2/public/values?alt=json";
 
 	$json = file_get_contents($url_inventory);
 	$data = json_decode($json, TRUE);
 	$rows = $data['feed']['entry'];
-	$total = 0;
 
 	foreach ($rows as $item) {
 		$id = $item['gsx$id']['$t'];
@@ -50,7 +69,6 @@ function getInventoryInfo() {
 		$inventory['categories'][$category][$id] = $vendor;
 		$inventory['vendors'][$id] = $vendor;
 	}
-	echo "inventory";
 }
 
 getAccountEmail();
@@ -59,7 +77,6 @@ if($VisibleCtl) {
 	getTemplateInfo();
 }
 ?>
-
 <style type="text/css">
 	#inventory, #cart {
 	    width: 100%;
@@ -148,220 +165,155 @@ if($VisibleCtl) {
 </style>
 
 <script>
-var Order_Deadline = 0
-var Special_Messages = ""
-var AccountEmail = ""
-var order = {}
-var vendors = {};//id, category, Vender, Type, Raw, Remit, First, Second, Third, forth
-var inventories = {'categories':{}, 'vendors': {}};
+	var Order_Deadline = "<?php echo $Order_Deadline  ?>";
+	var Special_Messages = '<?php echo $Special_Messages ?>';
+	var AccountEmail = "<?php echo $AccountEmail  ?>";
+	var order = {};
+//id, category, Vender, Type, Raw, Remit, First, Second, Third, forth
 
-var urls = {
-    'current_cart': "https://script.google.com/macros/s/AKfycbyFuHnEvzt3XTNc9Sy8R5KZldFVLU75jD1tDvL6l5ck6kJ6nS8Z/exec?alt=json&callback=updateInventory&Account=",
-    'template': "https://spreadsheets.google.com/feeds/list/1Kp0Lcneb_UUjE3Vi0FjpCNbXdTRLATjSpyNnLJx-E9Y/1/public/values?alt=json",
-    //'inventory': "https://spreadsheets.google.com/feeds/list/1Kp0Lcneb_UUjE3Vi0FjpCNbXdTRLATjSpyNnLJx-E9Y/2/public/values?alt=json&callback=populate",
-    'inventory': "https://spreadsheets.google.com/feeds/list/1Kp0Lcneb_UUjE3Vi0FjpCNbXdTRLATjSpyNnLJx-E9Y/2/public/values?alt=json",
-    'post': "https://script.google.com/macros/s/AKfycbyFuHnEvzt3XTNc9Sy8R5KZldFVLU75jD1tDvL6l5ck6kJ6nS8Z/exec"
-}
-function _populateInventory()
-{
-	var inventoryDetails = "";
-	for(category in inventories.categories) {
-		inventoryDetails += "<h2 align=\"left\">" + category + " </h2>";
+	var inventories = <?php echo json_encode( $inventory ) ?>;
+	function OnSelectPickupOption(){
 
-		for( v in inventories.categories[category]){
-			inventoryDetails += "<ul align=\"left\">" + v.name +" ," + v.first +"</ul>";
+	}
+	function OnSelectCategory(selectedVal) {
+		if(selectedVal === "disabled"){
+			return;
+		}
+		venderList = document.getElementById("vendor");
+		venderList.innerHTML = '';
+		for( v in inventories['categories'][selectedVal]){
+			var el = document.createElement("option");
+			var vendor = inventories["vendors"][v];
+			el.textContent = vendor[1];
+			el.value = v[0];
+			venderList.appendChild( el );
 		}
 	}
-	console.log(inventoryDetails);
-	document.getElementById("inventoryDetails").textContent = inventoryDetails;
-}
-function Vendor(id, name, category, type, raw, remit, first, second, third, fourth) {
-	this.id = id;
-	this.name = name;
-	this.category =  category;
-	this.type = type;
-	this.raw = raw;
-	this.remit = remit;
-	this.first = first;
-	this.second = secnd;
-	this.third = third;
-	this.fourth = fourth;
-};
-function _getInventory(json){
-	for (ix in json.feed.entry) {
-		vendorid = json.feed.entry[ix].gsx$id.$t;
-		category = json.feed.entry[ix].gsx$category.$t;
-		vendorName = json.feed.entry[ix].gsx$vendor.$t;
-		type = json.feed.entry[ix].gsx$type.$t;
-		raw = json.feed.entry[ix].gsx$raw.$t;
-		remit = json.feed.entry[ix].gsx$remit.$t;
-		first = json.feed.entry[ix].gsx$first.$t;
-		second = json.feed.entry[ix].gsx$second.$t
-		third = json.feed.entry[ix].gsx$third.$t;
-		fourth = json.feed.entry[ix].gsx$fourth.$t;
-		inventories.categories[ category ] = category;
-		//console.log(vendorid)
-		var v = { id:vendorid, name: vendorName, category:category, type:type, raw:raw, remit:remit, first:first, second:second, third:third, fourth:fourth};
+	function OnSelectVendor(selectedVal) {
 
-		if(!inventories.vendors.hasOwnProperty(category)){
-			inventories.vendors[category] ={};
-		}
-		inventories.vendors[category][v.name] = v;
 	}
-	_populateInventory();
-};
+	function addSelectedToOrder(){
+		var table = document.getElementById('orderList');
+		/*
+		var rowCount = table.rows.length;
+		var row = table.insertRow(rowCount);
 
-function loadJSON(path, success, error) {
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            if (xhr.status === 200) {
-                if (success)
-                    success(JSON.parse(xhr.responseText));
-            } else {
-                if (error)
-                    error(xhr);
-            }
-        }
-    }
-    ;
-    xhr.open("GET", path, true);
-    xhr.send();
-}
+		var cell3 = row.insertCell(0);
+		cell3.innerHTML="test"
+		*/
+		var listItem = document.createElement("li");
+		//create new text node
+		var textNode = document.createTextNode("test");
+		//add text node to li element
+		listItem.appendChild(textNode);
+		//add new list element built in previous steps to unordered list
+		table.appendChild(listItem);
+	}
+	function submitOrder(){
 
-function _getOrderDeadlineAndSpecialMessage(json) {
-    var pickupOption = document.getElementById("Pickup");
-    var specialMsgControl = document.getElementById("SpecialMessages");
-    var entrySize =json.feed.entry.length;
-    var count = 0;
-    for (ix in json.feed.entry) {
-        opt = json.feed.entry[ix].gsx$pickups.$t
-        opt = opt.trim();
-        if (opt.length > 0) {
-            //pickups.append(jQueryLatest("<option>").attr("value", opt).text(opt))
-            var optn = document.createElement("OPTION");
-            optn.text = opt;
-            optn.value = opt;
-            pickupOption.options.add(optn);
-            //console.log(opt)
-        }
-        if (json.feed.entry[ix].gsx$nextorderdate.$t) {
-            Order_Deadline = json.feed.entry[ix].gsx$nextorderdate.$t;
-        }
-		if (json.feed.entry[ix].gsx$specialmessages.$t) {
-			Special_Messages = json.feed.entry[ix].gsx$specialmessages.$t;
-        }
-        count++;
-        if(count == entrySize)
-        	break;
-        //console.log(count);
-        //console.log(Order_Deadline)
-       	// console.log(Special_Messages);
-    }
-    specialMsgControl.textContent = Special_Messages;
-    pickupOption.disabled = pickupOption.length === 0;
-    //console.log(json);
-    if(pickupOption.disabled == false)
-    {
-    loadJSON( urls['inventory'],
-		  function(data) { _getInventory(data); },
-		  function(xhr) {    console.error(xhr);}
-		);
-    }
-};
+	}
 
-
-
-loadJSON( urls['template'],
-		  function(data) { _getOrderDeadlineAndSpecialMessage(data); },
-		  function(xhr) {    console.error(xhr);}
-		);
-
-
-function changeAccount(e) {
-    email = prompt("User Account Email Please:");
-    document.getElementById("AccountEmail").textContent = email;
-
-    AccountEmail = email;
-    getCurrentOrder()
-}
 </script>
-<div>
+<body>
+<p id="order_html">
 <table border="0" bordercolor="#ccc" cellpadding="5" cellspacing="0" style="border-collapse:collapse;" summary="test">
 	<tbody>
 		<tr>
-			<td><span id="SpecialMessages"></span></td>
+			<td><span ><?php echo $Special_Messages; ?></span></td>
+		</tr>
+		<tr>
+			<td><span >Order deadline: <?php echo $Order_Deadline; ?></span></td>
+		</tr>
+		<tr>
+			<td><span >Please click <a href="https://www.teamunify.com/cansksc/UserFiles/File/Fundraising/2015-16/INSTRUCTIONSTOUSETHEGIFTCARDONLINEORDERINGSYSTEM.pdf">here</a> for instructions.</span></td>
 		</tr>
 	</tbody>
 </table>
 </div>
-
-<div>Please click <a href="https://www.teamunify.com/cansksc/UserFiles/File/Fundraising/2015-16/INSTRUCTIONSTOUSETHEGIFTCARDONLINEORDERINGSYSTEM.pdf">here</a> for instructions.</div>
 
 <hr />
 <form method="post" action="postdata.php">
 <table id="cart">
 	<thead>
 		<tr>
-			<td colspan="4">Account Email: <input id="setAccountBtn" onclick="changeAccount()" type="button" value="Set Account Email" /></td>
-			<td colspan="3">Pickup date/time<br />
-			<select id="Pickup"> <!-- 							<option value="" disabled selected>*Select Pickup Option</option> 							--> </select></td>
+			<td colspan="4">Account Email: <?php echo $AccountEmail;?></td>
+			<td colspan="3">Pickup date/time:
+			<select id="PickupOptions" onchange="OnSelectPickupOption(this.options[this.selectedIndex].value)">
+				<option value="disabled">==Select Pickup Option==</option>
+				<?php
+					foreach($PickupOptions as $option) {
+						echo '<option value="'. $option . '">'. $option .'</option>';
+					}
+				?>
+			</select></td>
 		</tr>
 		<tr>
-			<td colspan="3"><span id="AccountEmail"><?php echo $AccountEmail;?></span></td>
+			<th colspan="7"><hr/></th>
 		</tr>
+
 		<tr>
-			<th colspan="7">Current Gift Card Order:</th>
+			<th colspan="7">Please Choose and Add Cart. You can reference to inventories to see what are available</th>
 		</tr>
 		<tr >
+			<th>Category</th>
 			<th>Vendor</th>
-			<th>Card</th>
 			<th>Count</th>
 			<th>Subtotal</th>
 			<th>Remit</th>
 			<th>Recurring</th>
 			<th>&nbsp;</th>
 		</tr>
+
+		<tr>
+			<td>
+				<select  id="category" onChange="OnSelectCategory(this.options[this.selectedIndex].value)">
+						<option value="disabled">==Please Choose==</option>
+						<?php
+							foreach ($inventory['categories'] as $key => $value) {
+								echo '<option value="'. $key . '">'. $key .'</option>';
+							}
+					?>
+					</select>
+			</td><!--category-->
+			<td><select id="vendor"/></td><!--vendor-->
+			<td><input type="number" name="quantity" min="1" id="count" ></td><!--Count-->
+			<td witdh="10%"><input type="text" id="subtotal"/></td><!--Subtotal-->
+			<td witdh="100px"><input type="text" id="remit"/></td><!--Remit-->
+			<td witdh="10%"><input type="checkbox"  /></td><!--Recurring-->
+			<td><input onclick="addSelectedToOrder()" style="background: RGB(255,255,128);" type="button" value="Add To Order" /></td><!--Subtotal-->
+		</tr>
+
 	</thead>
 	<tfoot>
 		<tr>
 			<td>&nbsp;</td>
 		</tr>
+
 		<tr>
-			<td colspan="3" style="text-align: center;">Order:<br />
-			&nbsp;</td>
+			<td colspan="3" style="text-align: center;">Current Cart:</td>
 			<td id="cart_total_cost" style="text-align: right;">&nbsp;</td>
 			<td id="cart_total_remit" style="text-align: right;">&nbsp;</td>
-			<td colspan="2"><input onclick="submitCart()" style="background: RGB(128,255,128);" type="button" value="Update Order" /></td>
+			<td colspan="2"><input onclick="submitCart()" style="background: RGB(128,255,128);" type="button" value="Submit Order" /></td>
 		</tr>
 	</tfoot>
 	<tbody>
-		<tr>
-			<td colspan="7"><span class="loading">Please wait, updating cart.</span></td>
-		</tr>
-		<tr>
-			<td><select /></td><!--Vender-->
-			<td><select /></td><!--Card-->
-			<td><select /></td><!--Count-->
-			<td witdh="10%"><input type="text" /></td><!--Subtotal-->
-			<td witdh="100px"><input type="text" /></td><!--Remit-->
-			<td witdh="10%"><input type="checkbox"  /></td><!--Recurring-->
-			<td></td><!--Subtotal-->
+		<tr id="order_details">
+			<td><ul id="orderList"></ul></td>
+			<!--<td colspan="7"><span class="loading">Please wait, updating cart.</span></td>-->
 		</tr>
 	</tbody>
 </table>
 
-<p>&nbsp;</p>
-
-<p><br />
-<input checked="true" disabled="true" type="checkbox" />Recurring?</p>
-
-<p>&nbsp;</p>
-
+<!--
+<p>
+	<br />
+	<input checked="true" disabled="true" type="checkbox" />Recurring?
+</p>
+-->
 <table id="inventory">
 	<thead>
 		<tr>
-			<th colspan="4" id="categories"><h2>InVentories:</h2></th>
+			<th colspan="4" id="categories"><h2>Inventories:</h2></th>
 		</tr>
 		<!--
 		<tr>
@@ -387,3 +339,5 @@ function changeAccount(e) {
 	</tbody>
 </table>
 </form>
+</p>
+</body>
